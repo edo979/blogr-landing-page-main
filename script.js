2c5c9d6178231226d602973420e2047417b5a01d
edo979 <prezentabilan@gmail.com>
2022-03-04 01:03:45 +0100

    basic funcionality of navbar

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const navBtn = document.querySelector('.navbar-toggler'),
-  navEl = document.querySelector('.navbar')
+  navEl = document.querySelector('.navbar'),
+  dropdownBtnEls = document.querySelectorAll('.navbar .dropdown-toggle'),
+  dropdownEls = document.querySelectorAll('.navbar .dropdown')
 
-navBtn.addEventListener('click', (e) => {
-  navEl.classList.toggle('expanded')
+navBtn.addEventListener('click', (e) => navEl.classList.toggle('expanded'))
+
+dropdownBtnEls.forEach((dropdownBtn) => {
+  dropdownBtn.addEventListener('click', (e) => {
+    dropdownEls.forEach((dropdown) => dropdown.classList.remove('expanded'))
+
+    e.target.parentElement.classList.toggle('expanded')
+  })
 })
